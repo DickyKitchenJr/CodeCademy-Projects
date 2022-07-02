d96e7bf574f368c75ffc68809d274640d0d02971
DickyKitchenJr <80136142+DickyKitchenJr@users.noreply.github.com>
2022-07-02 12:40:22 -0400

    completed steps 74-80
    see README.md for where to find steps

---
+++ react-jammming-project/jammming/src/util/Spotify.js
@@ -0,0 +1,26 @@
+let accessToken;
+
+const Spotify = {
+    getAccessToken(){
+        if(accessToken){
+            return accessToken;
+        }
+
+        //check for access token match
+        const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+        if(accessTokenMatch && expiresInMatch){
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+
+            //clears the parameters so that a new access token can be grabbed once it expires
+            window.setTimeout(()=> accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        }
+
+    }
+};
+
+export default Spotify;
